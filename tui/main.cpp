68927de6d6c3a0b239a57e0233263fd96a8464fc
valentin rameau <valentin.rameau@outlook.fr>
2026-04-09 17:13:42 +0900

    a

--- tui/main.cpp
+++ tui/main.cpp
@@ -1,19 +1,18 @@
-#include "tui/my_app_component.h"
 #include "tui/mode_ui_mapping.h"
+#include "tui/my_app_component.h"
 
 #include <memory>
 
 int main() {
   std::shared_ptr<KeyboardService> service = std::make_shared<KeyboardService>();
   UiState initialState;
-  ModeData currentModeData;
+  ModeData currentModeData{};
   if (!service->getCurrentMode(currentModeData)) {
     return -1;
   }
   modeDataToUi(currentModeData, initialState);
   MyAppComponent myAppComponent = MyAppComponent(initialState, service);
-
-  // myAppComponent.loop();
+  myAppComponent.loop();
 
   return 0;
 }
